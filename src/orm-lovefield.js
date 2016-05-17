05e01b7f847de54b3700337bb0e40b3551d7176b
Julio Guerra <julio.war017@gmail.com>
2016-05-17 08:19:18 -0600

    fixing issue with insert and get all records

--- src/orm-lovefield.js
+++ src/orm-lovefield.js
@@ -1,71 +1,94 @@
 'use strict';
+if(!lf) {
+    alert('Verifica que se esta cargando lovefield');
+} else {
 
-// initialize all variables
-var ORM_lf = {};
-ORM_lf.connection = {};
+    // initialize all variables
+    var ORM_lf = {
+        connection: {
+            database:"",
+            version: 0,
+            createSchema: undefined
+        }
+    };
 
-ORM_lf.setConection = function(connection){
-    this.connection = Object.create(connection);
-};
+    ORM_lf.setConection = function (connection) {
+        this.connection = Object.create(connection);
+    };
 
-/*
-    Model Prototype
- */
-ORM_lf.Model = function(tableName){
-    this.tableName = tableName;
-};
+    ORM_lf.getConection = function(){
+        //console.log(ORM_lf.connection);
+        if(!ORM_lf.connection.database || !ORM_lf.connection.version){
+            throw "Verifica la configuracion de la base de datos.";
+        }
+        return lf.schema.create(ORM_lf.connection.database,ORM_lf.connection.version);
+    };
 
-ORM_lf.Model.prototype.getSchema = function(query){
-    var self = this;
-    return ORM_lf.connection.connect(function(db){
-        return query(db , db.getSchema().table(self.tableName));
-    });
-};
+    /*
+     Model Prototype
+     */
 
-ORM_lf.Model.prototype.query =  function(query){
-    return this.getSchema(function(db,table) {
-        var builder = db.select().from(table);
-        return query(db, builder);
-    });
-};
+    ORM_lf.Model = function (params) {
+        for(var key in params){
+            this[key] = params[key];
+        }
+    };
 
-ORM_lf.Model.prototype.all = function(){
-    return this.query(function(db,builder){
-        return builder.exec();
-    });
-};
-
-ORM_lf.Model.prototype.insert = function(rows){
-    return this.getSchema(function(db,table){
-
-        if(!Array.isArray(rows)){
-            rows = [
-                table.createRow(rows)
-            ];
+    ORM_lf.Model.prototype.getSchema = function (query) {
+        var self = this;
+        var builder = ORM_lf.getConection();
+        if(typeof(ORM_lf.connection.createSchema) != "undefined"){
+            ORM_lf.connection.createSchema(builder);
         } else {
-            for(var key in rows){
-                rows[key] = table.createRow(rows[key]);
-            }
+            throw "Se debe definir el Schema de la base de datos.";
         }
 
-        return db.insert().into(db).values(rows).exec();
-    });
-};
+        return builder.connect().then(function (db) {
+            return query(db, db.getSchema().table(self.tableName));
+        });
+    };
 
-// Relationship Prototypes
+    ORM_lf.Model.prototype.query = function (query) {
+        return this.getSchema(function (db, table) {
+            var builder = db.select().from(table);
+            return query(db, builder);
+        });
+    };
 
-ORM_lf.Model.prototype.belongsTo = function(Model,foreign_key,local_key){
-    return [];
-};
+    ORM_lf.Model.prototype.all = function () {
+        return this.query(function (db, builder) {
+            return builder.exec();
+        });
+    };
 
-ORM_lf.Model.prototype.belongsToMany = function(Model,table,foreign_key,local_key){
-    return [];
-};
+    ORM_lf.Model.prototype.insert = function (rows) {
+        return this.getSchema(function (db,table) {
 
-ORM_lf.Model.prototype.hasMany = function(Model,foreign_key,local_key){
-    return [];
-};
+            if(!Array.isArray(rows)){
+                rows = [
+                    table.createRow(rows)
+                ];
+            } else {
+                for(var key in rows){
+                    rows[key] = table.createRow(rows[key]);
+                }
+            }
+            return db.insertOrReplace().into(table).values(rows).exec();
+        });
+    };
 
+    // Relationship Prototypes
 
+    ORM_lf.Model.prototype.belongsTo = function (Model, foreign_key, local_key) {
+        return {};
+    };
 
+    ORM_lf.Model.prototype.belongsToMany = function (Model, table, foreign_key, local_key) {
+        return [];
+    };
 
+    ORM_lf.Model.prototype.hasMany = function (Model, foreign_key, local_key) {
+        return [];
+    };
+
+}
--- test/demo.js
+++ test/demo.js
@@ -1,17 +1,43 @@
-var schemaBuilder = lf.schema.create('todo', 1);
 
-schemaBuilder.createTable('Item').
-    addColumn('id', lf.Type.INTEGER).
-    addColumn('description', lf.Type.STRING).
-    addColumn('deadline', lf.Type.DATE_TIME).
-    addColumn('done', lf.Type.BOOLEAN).
-    addPrimaryKey(['id']).
-    addIndex('idxDeadline', ['deadline'], false, lf.Order.DESC);
-
-ORM_lf.setConection(schemaBuilder);
-
-var Item = new ORM_lf.Model("item");
-
-Item.all().then(function(data){
-    console.log(data);
+var User = new ORM_lf.Model({
+    tableName:"User",
+    items: function(){
+        return this.hasMany(Item);
+    }
 });
+
+var Item = new ORM_lf.Model({
+    tableName:'Item',
+    user: function(){
+        return this.belongsTo(User)
+    }
+});
+ORM_lf.setConection({
+    database: "todo",
+    version: 1,
+    createSchema: function(builder){
+        // Creating the schema of database
+        builder.createTable(User.tableName).
+        addColumn('id', lf.Type.INTEGER).
+        addColumn('name', lf.Type.STRING).
+        addColumn('last_name', lf.Type.STRING).
+        addPrimaryKey(['id']);
+
+        builder.createTable(Item.tableName).
+        addColumn('id', lf.Type.INTEGER).
+        addColumn('description', lf.Type.STRING).
+        addColumn('deadline', lf.Type.DATE_TIME).
+        addColumn('done', lf.Type.BOOLEAN).
+        addColumn('userId',lf.Type.INTEGER).
+        addPrimaryKey(['id']).
+        addForeignKey('fk_userId',{
+            local: 'userId',
+            ref: 'User.id'
+        });
+    }
+});
+
+
+//var newUser = new User;
+
+//console.log(newUser);
